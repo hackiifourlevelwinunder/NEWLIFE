c2b0f5455193667a859cfe79a24f70aa946d4109
hackiifourlevelwinunder <pg.ankushkumar@gmail.com>
2026-03-12 12:42:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import random
 from datetime import datetime, timedelta
 import pytz
 from flask import Flask, jsonify, render_template
@@ -11,8 +12,8 @@ FIXED_PART = "10001"
 RESET_HOUR = 5
 RESET_MINUTE = 30
 
-# result storage
 round_results = {}
+preview_numbers = {}
 
 def get_reset_time(now):
     reset_time = now.replace(hour=RESET_HOUR, minute=RESET_MINUTE, second=0, microsecond=0)
@@ -37,32 +38,29 @@ def get_round_info():
     return round_id, remaining
 
 
-def generate_result(round_id):
+def generate_preview(round_id):
 
-    now = datetime.now(IST)
+    seed = int(round_id[-4:])
+    random.seed(seed)
 
-    minute = now.minute
-    second = now.second
+    numbers = []
 
-    rng1 = (minute + second + int(round_id[-1])) % 10
-    rng2 = (minute * 2 + second + int(round_id[-2])) % 10
-    rng3 = (minute + second * 2 + int(round_id[-3])) % 10
+    for _ in range(21):
+        numbers.append(random.randint(0,9))
 
-    numbers = [rng1, rng2, rng3]
+    return numbers
 
-    small = [n for n in numbers if n <= 4]
-    big = [n for n in numbers if n >= 5]
 
-    if len(small) > len(big):
-        group = [0,1,2,3,4]
-    elif len(big) > len(small):
-        group = [5,6,7,8,9]
-    else:
-        group = [0,1,2,3,4,5,6,7,8,9]
+def calculate_result(numbers):
 
-    index = (minute + second + sum(numbers)) % len(group)
+    freq = {}
 
-    return group[index]
+    for n in numbers:
+        freq[n] = freq.get(n,0) + 1
+
+    result = max(freq, key=freq.get)
+
+    return result
 
 
 @app.route("/")
@@ -75,24 +73,31 @@ def api():
 
     round_id, remaining = get_round_info()
 
-    # result generate only once
+    # preview start (40 sec)
     if remaining <= 40:
 
+        if round_id not in preview_numbers:
+            preview_numbers[round_id] = generate_preview(round_id)
+
+        numbers = preview_numbers[round_id]
+
         if round_id not in round_results:
-            round_results[round_id] = generate_result(round_id)
+            round_results[round_id] = calculate_result(numbers)
 
         result = round_results[round_id]
 
     else:
+        numbers = []
         result = None
 
     return jsonify({
         "round_id": round_id,
         "remaining": remaining,
+        "preview_numbers": numbers,
         "result": result
     })
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port)
