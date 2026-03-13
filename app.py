f2c7a2c7181110ca57eddd65ac70527e417c6f0a
hackiifourlevelwinunder <pg.ankushkumar@gmail.com>
2026-03-13 23:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 ROUND_TIME = 60
 PREVIEW_TIME = 40
 
-SERVER_SEED = "ankush_secret_seed_2026"
+SERVER_SEED = "ankush_rng_secret"
 
 
 def get_reset_time():
@@ -33,9 +33,7 @@ def get_round():
 
     seconds = int(diff.total_seconds())
 
-    round_number = seconds // 60 + 1
-
-    return round_number
+    return seconds // 60 + 1
 
 
 def get_period():
@@ -44,9 +42,7 @@ def get_period():
 
     round_number = get_round()
 
-    period = f"{date}10001{round_number:04d}"
-
-    return period
+    return f"{date}10001{round_number:04d}"
 
 
 def get_time_left():
@@ -60,34 +56,9 @@ def generate_number(period):
 
     data = f"{SERVER_SEED}-{period}"
 
-    hash_value = hashlib.sha256(data.encode()).hexdigest()
+    h = hashlib.sha256(data.encode()).hexdigest()
 
-    number = int(hash_value, 16) % 10
-
-    return number
-
-
-def get_color(n):
-
-    if n in [1,3,7,9]:
-        return "Green"
-
-    if n in [2,4,6,8]:
-        return "Red"
-
-    if n == 0:
-        return "Red+Violet"
-
-    if n == 5:
-        return "Green+Violet"
-
-
-def get_size(n):
-
-    if n >= 5:
-        return "Big"
-    else:
-        return "Small"
+    return int(h, 16) % 10
 
 
 @app.route("/")
@@ -105,30 +76,13 @@ def result():
 
     number = generate_number(period)
 
-    if time_left <= PREVIEW_TIME:
-        show_number = number
-    else:
-        show_number = None
-
-    if show_number is not None:
-
-        color = get_color(show_number)
-
-        size = get_size(show_number)
-
-    else:
-
-        color = None
-        size = None
+    if time_left > PREVIEW_TIME:
+        number = None
 
     return jsonify({
-
         "period": period,
         "time_left": time_left,
-        "number": show_number,
-        "color": color,
-        "size": size
-
+        "number": number
     })
 
 
